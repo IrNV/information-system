4a3f1d5f0d5aff256270948977a360eda4e9d22b
Valera Ryabchinskiy <NsT.valerka@ukr.net>
2017-12-09 03:34:50 +0200

    Create archive window

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 # Импортируем наш интерфейс из файла
 from main_window import *
 from client_window import *
+from archive_window import *
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
@@ -11,6 +12,7 @@ from PyQt5.QtCore import QSize, Qt
 
 class MyWin(QtWidgets.QMainWindow):
     __clients = None
+    __archive = None
 
     def __init__(self, parent=None):
         QtWidgets.QWidget.__init__(self, parent)
@@ -19,10 +21,15 @@ class MyWin(QtWidgets.QMainWindow):
 
         # Здесь прописываем событие нажатия на кнопку
         self.ui.pushButton.clicked.connect(self.function)
+        self.ui.pushButton_3.clicked.connect(self.f)
 
     def function(self):
         self.__clients = ClientWindow()
         self.__clients.show()
+
+    def f(self):
+        self.__archive = ArchiveWindow()
+        self.__archive.show()
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
